d0c010ba44a55a938e87d427ae20f59e9d0d6fdf
Gorka Magra <gorka.magra@gmail.com>
2018-06-19 14:13:33 +0200

    -Citizen and zombie prefabs created (not the model, just the logic) -First aproach to NPC AI. -Both of the trees generated, still not a "flee" functionality.

---
+++ Assets/Scripts/CitizenBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitizenBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Hit()        //The Citizen gets hit by a bullet or car and dies.
+    {
+        //TODO Add Score (?)
+        StartCoroutine(DyingSequence());
+    }
+
+    public void Zombified() //Zombie bites a citizen.
+    {
+        StartCoroutine(DyingSequence());
+        //TODO Instantiate a zombie in this position.
+    }
+
+    public void Flee()
+    {
+        Debug.Log("Corre pequeño, HUYE");
+    }
+
+
+
+    IEnumerator DyingSequence()
+    {
+        //TODO DyingSound
+        //TODO Dying animation
+        yield return new WaitForSeconds(3.0f);
+        Destroy(this.gameObject);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Bullet") || (other.gameObject.tag.Equals("Car")))
+            Hit();
+
+        if (other.gameObject.tag.Equals("Zombie"))
+            Zombified();
+    }
+
+}
