d084c2b98244f06ff6b0ec7b96f933fb3ee700cf
Gorka Magra <gorka.magra@gmail.com>
2018-06-20 13:20:18 +0200

    -Finished AI from citizens and zombies. -Added "flee" functionality

--- Assets/Scripts/CitizenBehaviour.cs
+++ Assets/Scripts/CitizenBehaviour.cs
@@ -1,13 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CitizenBehaviour : MonoBehaviour {
 
+    private GameObject nearestZombie;
+    private GameObject[] enemies;
+    private float distance;
+    private Transform startTransform;
+    private NavMeshAgent myNMAgent;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+        myNMAgent = this.GetComponent<NavMeshAgent>();
+
+    }
 
     public void Hit()        //The Citizen gets hit by a bullet or car and dies.
     {
@@ -24,6 +34,41 @@ public class CitizenBehaviour : MonoBehaviour {
     public void Flee()
     {
         Debug.Log("Corre pequeño, HUYE");
+        nearestZombie = getClosestEnemy();
+
+
+        startTransform = transform;
+        transform.rotation = Quaternion.LookRotation(transform.position - nearestZombie.transform.position);
+        Vector3 runTo = transform.position + transform.forward;
+
+        NavMeshHit hit;
+        NavMesh.SamplePosition(runTo, out hit, 5, 1 << NavMesh.GetAreaFromName("Default"));
+        transform.position = startTransform.position;
+        transform.rotation = startTransform.rotation;
+
+        myNMAgent.SetDestination(hit.position);
+
+
+    }
+
+    private GameObject getClosestEnemy()
+    {
+        distance = Mathf.Infinity;
+        enemies = GameObject.FindGameObjectsWithTag("Zombie");
+        GameObject chosenZombie = null;
+
+        foreach(GameObject z in enemies)
+        {
+            float dist = Vector3.Distance(z.transform.position, transform.position);
+
+            if(dist < distance)
+            {
+                chosenZombie = z;
+            }
+
+        }
+
+        return chosenZombie;
     }
 
 
