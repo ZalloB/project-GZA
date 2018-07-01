b09197ee499e23ec5dc34f934e6cd79851c1d775
Beatriz <beatriz.zallo@gmail.com>
2018-07-01 23:08:39 +0200

    fix camera mult and movement

---
+++ Assets/Scripts/ManagePlayersData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ManagePlayersData : NetworkBehaviour {
+
+    public GameObject MyCamera;
+
+	// Use this for initialization
+	void Start () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        MyCamera.SetActive(true);
+        this.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter>().enabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+    }
+}
