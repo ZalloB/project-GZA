8c478ec6cde2dafdf0ab8deee0a17b393998beb2
Gorka Magra <gorka.magra@gmail.com>
2018-07-02 01:22:58 +0200

    -Added game behaviour to the main scene. -Added zombie spawn behaviour. -Modified the scene to put NPC (cars, citizens) -Modified citizen behavior tree.

---
+++ Assets/Scripts/GameBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBehaviour : MonoBehaviour {
+
+    private float SpawnRate = 8.05f;
+    private GameObject[] spawnPoints;
+    private GameObject chosenSpawnPoint;
+    private int index;
+    public GameObject zombie;
+
+    private void Awake()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public IEnumerator SpawnEnemies()
+    {
+
+        while (true)
+        {
+
+            if (SpawnRate > 0.5f)
+            {
+                SpawnRate -= 0.05f;
+            }
+
+            spawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
+            index = UnityEngine.Random.Range(0, spawnPoints.Length);
+            chosenSpawnPoint = spawnPoints[index];
+
+            Instantiate(zombie, chosenSpawnPoint.transform.position, chosenSpawnPoint.transform.rotation);
+
+
+            yield return new WaitForSeconds(SpawnRate);
+        }
+
+
+    
+    }
+}
+
+
+
+
+
